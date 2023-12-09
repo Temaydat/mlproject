4c9545a68faebfb1da67cd18738f87dc5ced288c
Mary Temmy <temaydat@gmail.com>
2023-12-09 22:01:14 +1000

    Prediction Pipline

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 import dill # helps to create the pkl file
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
  
@@ -36,7 +37,15 @@ def evaluate_model(X_train, y_train, X_test, y_test, models,param):
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
             report[list(models.keys())[i]] = test_model_score
-            
+
         return report
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
